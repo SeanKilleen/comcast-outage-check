8c70c7664fdbf0b58adf39dcafab13d451494d85
Sean Killeen <SeanKilleen@gmail.com>
2020-04-21 08:36:48 -0400

    initial script

---
+++ script/OutageCheck.ps1
@@ -0,0 +1,27 @@
+$unfilteredLogName = "syslog.log"
+$filteredLogName = "filteredSyslog.log"
+$csvName = "logs.csv"
+
+Remove-Item -Force $filteredLogName -ErrorAction Ignore
+
+Get-Content $unfilteredLogName | Select-String -Pattern "WAN1_(OFF|ON)" | Set-Content $filteredLogName
+
+$filteredLogLines = Get-Content $filteredLogName | Select-Object -Property @{Name = 'Date'; Expression = {$_.Substring(0,19)}}, @{Name = 'OnOff'; Expression = {$_.Substring($_.IndexOf("WAN1_") + 5,2)}}
+
+$deduped = @()
+for ($i = 0; $i -lt $filteredLogLines.Length; $i++){
+    $line = $filteredLogLines[$i]
+    if ($i -eq 0){
+        $deduped+= $line
+        continue
+    }
+    
+    $latestDedupedItem = $deduped[-1]
+    if ($latestDedupedItem.OnOff -ne $line.OnOff){
+        $deduped+= $line
+    }
+}
+
+$deduped
+
+$deduped | Export-Csv $csvName -NoTypeInformation
