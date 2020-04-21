e940259f9f3c2da56ed4a7fc38559c70a701695e
Sean Killeen <SeanKilleen@gmail.com>
2020-04-21 08:57:46 -0400

    Parse a date object

--- script/OutageCheck.ps1
+++ script/OutageCheck.ps1
@@ -6,7 +6,7 @@ Remove-Item -Force $filteredLogName -ErrorAction Ignore
 
 Get-Content $unfilteredLogName | Select-String -Pattern "WAN1_(OFF|ON)" | Set-Content $filteredLogName -Force
 
-$filteredLogLines = Get-Content $filteredLogName | Select-Object -Property @{Name = 'Date'; Expression = {$_.Substring(0,19)}}, @{Name = 'OnOff'; Expression = {$_.Substring($_.IndexOf("WAN1_") + 5,2)}}
+$filteredLogLines = Get-Content $filteredLogName | Select-Object -Property @{Name = 'Date'; Expression = {[datetime]::ParseExact($_.Substring(0,19), "yyyy-MM-dd HH:mm:ss", $null)}}, @{Name = 'OnOff'; Expression = {$_.Substring($_.IndexOf("WAN1_") + 5,2)}}
 
 $deduped = @()
 for ($i = 0; $i -lt $filteredLogLines.Length; $i++){
